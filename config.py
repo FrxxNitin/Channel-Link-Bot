0c868e032bef8cc3b4f38a1134e6461482c9779b
FrxxNitin <nitinkumardhundhara@gmail.com>
2025-08-05 14:57:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 22225430
 API_HASH = "4c5c28abd62233ef4b993fb972f83262"
-BOT_TOKEN = "7899"
-MONGO_URI = ""
+BOT_TOKEN = "8351272953:AAF5OnfXyzXAEAopE3s0zkiP05SzQdRVl-o"
+MONGO_URI = "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 ADMINS = [6039119180]  # Replace with your Telegram user ID(s)
 START_PIC = "https://i.ibb.co/4RXmH8dm/x.jpg"
 LINK_PIC = "https://i.ibb.co/r2ZC2YFT/x.jpg"
